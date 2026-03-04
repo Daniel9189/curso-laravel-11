74e066e450cbf93e4fd333e782805fd5b69c47fa
Daniel9189 <dbrito4321@gmail.com>
2026-03-04 20:49:45 -0300

    create partials folder

--- resources/views/adm/users/create.blade.php
+++ resources/views/adm/users/create.blade.php
@@ -7,13 +7,9 @@
 
     {{-- @include('admin.includes.errors') --}}
 
-    <x-alert-test/>
-
     <form action="{{ route('users.store') }}" method="POST">
-        @csrf
-        <input type="text" name="name" placeholder="Nome" value="{{ old('name') }}">
-        <input type="email" name="email" placeholder="E-mail" value="{{ old('email') }}">
-        <input type="password" name="password" placeholder="Senha">
-        <button type="submit">Enviar</button>
+
+        @include('adm.users.partials.form')
+        
     </form>
 @endsection
--- resources/views/adm/users/edit.blade.php
+++ resources/views/adm/users/edit.blade.php
@@ -6,22 +6,9 @@
 
     <h2>Editar Usuário {{ $user->name }}</h2>
 
-    @if ($errors->any)
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
-
-
     <form action="{{ route('users.update', $user->id) }}" method="POST">
-        @csrf
         @method('put')
-        <input type="text" name="name" placeholder="Nome" value="{{ $user->name }}">
-        <input type="email" name="email" placeholder="E-mail" value="{{ $user->email }}">
-        <input type="password" name="password" placeholder="Senha">
-        <button type="submit">Enviar</button>
+        @include('adm.users.partials.form')
     </form>
 
 @endsection
